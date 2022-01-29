f17a4604ea46f1355eec7ed088efd3d2484515d8
Chocolatey <chocolatey@realdimensions.net>
2022-01-29 01:14:13 +0000

    AU: 1 updated - notion [skip ci] https://gist.github.com/b03afd2cc105a41fb762ff9f84e76d3a/6713c77e32b7a77a0f2c481eb299e32828d361d3

--- notion/tools/chocolateyinstall.ps1
+++ notion/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url64      = 'https://desktop-release.notion-static.com/Notion%20Setup%202.0.18.exe'
+$url64      = 'https://desktop-release.notion-static.com/Notion%20Setup%202.0.21.exe'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'EXE'
   url64bit       = $url64
   softwareName   = 'notion*'
-  checksum64     = 'aa584eb9766c73c374bb24c53e16edb7c343a9e2f03254a84c4bdb6e2be79bb7'
+  checksum64     = 'aa2d0725661f8e132df46f07773cb5e79aa8f7ad33aa370e0bd8823be6b02156'
   checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
