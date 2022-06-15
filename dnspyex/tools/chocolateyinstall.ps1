6880d26be5fc046aca3cde01b012610634400522
Ryan Caezar Itang <sitiom@disroot.org>
2022-06-15 08:18:22 +0800

    fix(dnspyex): do not double unzip
    As of 6.2.0, it is no longer double zipped

--- dnspyex/tools/chocolateyinstall.ps1
+++ dnspyex/tools/chocolateyinstall.ps1
@@ -2,23 +2,15 @@
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
-# Extract twice because of nested zips
-$packageArgs = @{
-  fileFullPath   = Get-Item $toolsDir\*-win32.zip
-  fileFullPath64 = Get-Item $toolsDir\*-win64.zip
-  destination    = "$toolsDir\temp"
-}
-Get-ChocolateyUnzip @packageArgs
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileFullPath   = Get-Item $toolsDir\temp\*-win32.zip
-  fileFullPath64 = Get-Item $toolsDir\temp\*-win64.zip
+  fileFullPath   = Get-Item $toolsDir\*-win32.zip
+  fileFullPath64 = Get-Item $toolsDir\*-win64.zip
   destination    = $toolsDir
 }
 Get-ChocolateyUnzip @packageArgs
 
 # Zip not needed anymore
-Remove-Item $toolsDir\temp -Recurse -Force -ea 0 
 Remove-Item $toolsDir\*.zip -Force -ea 0 
 
 $file = Get-Childitem $toolsDir -Include dnSpy.exe -Recurse
